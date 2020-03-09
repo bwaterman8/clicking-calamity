73bfd992ac8f8d0442649599fcd8e340e20297af
bwaterman8 <bwaterman8@gmail.com>
2020-03-09 17:44:41 -0400

    commmented everything out and started from scratch except for the modals and link to site. made many queryselectors and functions all on app.js

--- src/js/ClickCounter.js
+++ src/js/ClickCounter.js
@@ -1,66 +1,69 @@
-class ClickCount {
-    constructor() {
-        this.companionCost = 100;    
-        this.clickCount = 0;
-        this.companionCount = 0;
-        this.culminationCount = 0;
-        this.culminationCost = 10;
-    }
-    activateCompanion() {
-        for(let i = 0; i<this.clickCompanionCount; i++) {
-            this.clickCookie();
-        }
-    }
-    clickCookie() {
-        this.clickCount += 1 * this.valueMultiplier;
-    }
+//  class ClickCount {
+    //  constructor() {
+//         this.companionCost = 100;    
+    //    this.clickCount = 0;
+//         this.companionCount = 0;
+//         this.culminationCount = 0;
+//         this.culminationCost = 10;
+//    }
+//     displayClickCount() {
+//         this.clickCount++;
+//     }
+//     activateCompanion() {
+//         for(let i = 0; i<this.clickCompanionCount; i++) {
+//             this.clickCookie();
+//         }
+//     }
+//     clickCookie() {
+//         this.clickCount += 1 * this.valueMultiplier;
+//     }
 
-    countClick() {
-       return this.clickCount++;
-    }
+    //  countClick() {
+    //     return this.clickCount++;
+    //  }
 
-    clickCompanionCount() {
-        return 0;
-    }
-    clickCompanion() {
-        this.companionCount++;
-    }
-    clickCompanionPurchase() {
-       this.clickCount -= this.companionCost;
-       this.increaseClickingCompanionsCost;
-       if (this.clickCount >= this.companionCost) {
-        this.companionCost = Math.round(1.1 * this.companionCost);
-       }
-    }
-    increaseClickingCompanionsCost() {
-            this.companionCost = Math.round(1.1 * this.companionCost);
-    }
-    clickCompanionAddsToClickCount() {
-        this.clickCount++ + this.companionCount++;
-    }
-    culminationCompounderCount() {
-        return 0;
-    }
-    culminationCompounder() {
-        this.culminationCount++;
-        if(this.culminationCompounderPurchase) {
-            this.clickCount += 1 * this.valueMultiplier();
-        }
-        if (this.culminationCompounderPurchase) {
-            this.companionCount += 1 * this.valueMultiplier();
-        }
-    }
-    valueMultiplier() {
-        return Math.pow(1.2, this.culminationCount);
-    }
-    culminationCompounderPurchase() {
-        this.clickCount -= this.culminationCost;
-        this.increaseCulminationCompounderCost;
-        if (this.clickCount >= this.culminationCost) {
-            this.culminationCost = Math.round(1.1 * this.culminationCost)
-        }
-    }
-    increaseCulminationCompounderCost() {
-        this.culminationCost = Math.round(1.1 * this.culminationCost);
-    }
-}
+//     clickCompanionCount() {
+//         return 0;
+//     }
+//     clickCompanion() {
+//         this.companionCount++;
+//     }
+//     clickCompanionPurchase() {
+//        this.clickCount -= this.companionCost;
+//        this.increaseClickingCompanionsCost;
+//        if (this.clickCount >= this.companionCost) {
+//         this.companionCost = Math.round(1.1 * this.companionCost);
+//        }
+//     }
+//     increaseClickingCompanionsCost() {
+//             this.companionCost = Math.round(1.1 * this.companionCost);
+//     }
+//     clickCompanionAddsToClickCount() {
+//         this.clickCount++ + this.companionCount++;
+//     }
+//     culminationCompounderCount() {
+//         return 0;
+//     }
+//     culminationCompounder() {
+//         this.culminationCount++;
+//         if(this.culminationCompounderPurchase) {
+//             this.clickCount += 1 * this.valueMultiplier();
+//         }
+//         if (this.culminationCompounderPurchase) {
+//             this.companionCount += 1 * this.valueMultiplier();
+//         }
+//     }
+//     valueMultiplier() {
+//         return Math.pow(1.2, this.culminationCount);
+//     }
+//     culminationCompounderPurchase() {
+//         this.clickCount -= this.culminationCost;
+//         this.increaseCulminationCompounderCost;
+//         if (this.clickCount >= this.culminationCost) {
+//             this.culminationCost = Math.round(1.1 * this.culminationCost)
+//         }
+//     }
+//     increaseCulminationCompounderCost() {
+//         this.culminationCost = Math.round(1.1 * this.culminationCost);
+//     }
+// }
--- src/js/app.js
+++ src/js/app.js
@@ -1,57 +1,80 @@
-const runGame = new ClickCount();
-startGame();
-gameLoop();
+// let startApp = new runGame();
+// class runGame {
+//     constructor(){
+// this.clickCount = 0;
+//     }
+// }
+// function displayClickCount() {
+//     return this.clickCount;
+// }
 
-function startGame() {
-    setUpClickers();
-    updateCount();
-}
+//  const runGame = new ClickCount();
+//     document.querySelector("cookies_count");
 
-function setUpClickers() {
-    setUpCountCookieButton();
-    setUpClickCompanionButton();
-    setUpCulminationCompounderButton();
-}
-function setUpCountCookieButton() {
-    const cookieCountButton = document.querySelector('.cookie_button');
-    cookieCountButton.innerText= `click for cookies. click value: ${runGame.valueMultiplier()}`
-    cookieCountButton.addEventListener('click', ()=>{
-        runGame.clickCookie();
-        cookieCountUpdater();
-    })
-}
-function setUpClickCompanionButton() {
-    const cookieCompanionButton = document.querySelector('.companion_button');
-    cookieCompanionButton.innerText= `click for a companion. companion cost: ${runGame.increaseClickingCompanionsCost()}`
-    cookieCompanionButton.addEventListener('click', ()=>{
-        cookieCountUpdater();
-        companionCountUpdater();
-    })
-}
-function setUpCulminationCompounderButton() {
-    const cookieCompounderButton = document.querySelector('.culmination_button');
-    cookieCompounderButton.innerText= `click to compound. culmination compounder cost: ${runGame.increaseCulminationCompounderCost()}`
-    cookieCompounderButton.addEventListener('click', ()=>{
-        cookieCountUpdater();
-        compounderCountUpdater();
-        clickCookieUpdater();
-    })
-}
-function cookieCountUpdater() {
-    const cookieCounting = document.querySelector('.cookies_count');
-    cookieCounting.innerText= runGame.countClick();
-}
-function companionCountUpdater() {
-    const companionCounting = document.querySelector('.companion_count');
-    companionCounting.innerText= runGame.clickCompanionCount();
-}
-function compounderCountUpdater() {
-    const compounderCounting = document.querySelector('.culmination_count');
-    compounderCounting.innerText= runGame.culminationCompounderCount();
-}
+
+// startGame();
+// gameLoop();
+
+// function startGame() {
+//     setUpClickers();
+//     updateCount();
+// }
+
+// function setUpClickers() {
+//     setUpCountCookieButton();
+//     setUpClickCompanionButton();
+//     setUpCulminationCompounderButton();
+// }
+// function setUpClickCompanionButton() {
+    //     const cookieCompanionButton = document.querySelector('.companion_button');
+    //     cookieCompanionButton.innerText= `click for a companion. companion cost: ${runGame.increaseClickingCompanionsCost()}`
+    //     cookieCompanionButton.addEventListener('click', ()=>{
+        //         cookieCountUpdater();
+        //         companionCountUpdater();
+        //     })
+        // }
+        // function setUpCulminationCompounderButton() {
+            //     const cookieCompounderButton = document.querySelector('.culmination_button');
+            //     cookieCompounderButton.innerText= `click to compound. culmination compounder cost: ${runGame.increaseCulminationCompounderCost()}`
+            //     cookieCompounderButton.addEventListener('click', ()=>{
+                //         cookieCountUpdater();
+                //         compounderCountUpdater();
+                //         clickCookieUpdater();
+                //     })
+//  function setUpCountCookieButton() {
+    //  const cookieCountButton = document.querySelector('.cookie_button');
+    //  cookieCountButton.innerText= `click for cookies. click value: ${runGame.valueMultiplier()}`
+    //  cookieCountButton.addEventListener('click', ()=>{
+        //  runGame.clickCookie();
+        //  cookieCountUpdater();
+    //  })
+//  }
+// }
+// function cookieCountUpdater() {
+//     const cookieCounting = document.querySelector('.cookies_count');
+//     cookieCounting.innerText= runGame.countClick();
+// }
+// function companionCountUpdater() {
+//     const companionCounting = document.querySelector('.companion_count');
+//     companionCounting.innerText= runGame.clickCompanionCount();
+// }
+// function compounderCountUpdater() {
+//     const compounderCounting = document.querySelector('.culmination_count');
+//     compounderCounting.innerText= runGame.culminationCompounderCount();
+// }
+
+
+let clickCount = 0;
+let companionCount = 0;
+let culminationCount = 0;
+let companionCost = 100;
+let culminationCost = 10;
+let valueMultiplier = Math.pow(1.2, clickCount);
+
 document.querySelector('.modal_one_open').addEventListener('click', ()=> {
     document.querySelector('.modal_one_background').style.display = 'flex';
 })
+
 document.querySelector('.modal_one_close').addEventListener('click', ()=> {
     document.querySelector('.modal_one_background').style.display = 'none';
 })
@@ -61,3 +84,43 @@ document.querySelector('.modal_two_open').addEventListener('click', ()=> {
 document.querySelector('.modal_two_close').addEventListener('click', ()=> {
     document.querySelector('.modal_two_background').style.display = 'none';
 })
+
+document.querySelector('.cookies_count').innerText = clickCount;
+document.querySelector('.companion_count').innerText = companionCount;
+document.querySelector('.culmination_count').innerText = culminationCount;
+
+
+document.querySelector('.cookie_button').addEventListener('click', cookieClick)
+document.querySelector('.companion_button').addEventListener('click', companionClick)
+document.querySelector('.culmination_button').addEventListener('click', culminationClick)
+
+
+
+document.querySelector('.cookie_button').innerText= `Click for cookies! Click Value: ${valueMultiplier}`;
+document.querySelector('.companion_button').innerText= `Click for a clicking companion! Companion cost: ${companionCost}`;
+document.querySelector('.culmination_button').innerText= `Click for a culmination compounder! Compounder cost: ${culminationCost}`;
+document.querySelector('.culmination_button').addEventListener('click', culminationClickDecreasesClickCount);
+document.querySelector('.culmination_button').addEventListener('click', culminationClickIncreasesCulminationCost);
+
+
+function cookieClick() {
+    clickCount++;
+    document.querySelector('.cookies_count').innerText = clickCount;
+
+}
+function companionClick() {
+    companionCount++;
+    document.querySelector('.companion_count').innerText = companionCount;
+}
+function culminationClick() {
+    culminationCount++;
+    document.querySelector('.culmination_count').innerText = culminationCount;
+}
+function culminationClickDecreasesClickCount() {
+    clickCount -= culminationCost;
+    document.querySelector('.cookies_count').innerText = clickCount;
+}
+function culminationClickIncreasesCulminationCost () {
+    culminationCost++;
+    document.querySelector('.culmination_button').innerText = `Click for a culmination compounder! Compounder cost: ${culminationCost}`;
+}
